23452f2ae6708a21163654e6531f1e5206fe188e
Carlos Enrique Lemus Serrano <cwlemus@gmail.com>
2021-02-27 21:47:16 -0600

    Agregar archivos de proyecto.

---
+++ Server/Controllers/GradoController.cs
@@ -0,0 +1,165 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RegistroAcademicoApp.Server.Models;
+using RegistroAcademicoApp.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RegistroAcademicoApp.Server.Controllers
+{
+    [ApiController]
+    public class GradoController : Controller
+    {
+
+        [HttpGet]
+        [Route("api/Grado/obtenerGrado/{idGrado}")]
+        public GradoCls obtenerGrado(int idGrado)
+        {
+            GradoCls clteCls = new GradoCls();
+            using (RegistroAcademicoContext db = new RegistroAcademicoContext())
+            {
+                clteCls = (from grado in db.Grado
+                           where grado.GradoId == idGrado
+                           select new GradoCls
+                           {
+                               GradoId = grado.GradoId,
+                               GradoNombre = grado.GradoNombre,
+                               Seccion = grado.Seccion
+
+                           }).First();
+
+            }
+            return clteCls;
+        }
+
+        [HttpPost]
+        [Route("api/Grado/Guardar")]
+        public async Task<ActionResult<int>> Guardar(GradoCls gradoCls)
+        {
+
+            int rpta = 0;
+            try
+            {
+
+                using (RegistroAcademicoContext db = new RegistroAcademicoContext())
+                {
+                    Grado oGrado = new Grado();
+                    if (gradoCls.GradoId == 0)
+                    {
+                        oGrado.GradoId = gradoCls.GradoId;
+                        oGrado.GradoNombre = gradoCls.GradoNombre;
+                        oGrado.Seccion = gradoCls.Seccion;
+                        db.Grado.Add(oGrado);
+                    }
+                    else
+                    {
+                        Grado g = db.Grado.Where(g => g.GradoId == gradoCls.GradoId).FirstOrDefault();
+                        g.GradoNombre = gradoCls.GradoNombre;
+                        g.Seccion = gradoCls.Seccion;
+                    }
+                    await db.SaveChangesAsync();
+                    rpta = 1;
+                }
+
+
+            }
+            catch (Exception)
+            {
+                rpta = 0;
+            }
+            return rpta;
+        }
+
+
+
+
+
+        [HttpGet]
+        [Route("api/Grado/EliminarGrado/{data?}")]
+        public int eliminarGrado(string data)
+        {
+            int rpta = 0;
+            try
+            {
+                using (RegistroAcademicoContext db = new RegistroAcademicoContext())
+                {
+                    int idGrado = int.Parse(data);
+                    Grado grado = db.Grado.Where(g => g.GradoId == idGrado).First();
+                    db.Attach(grado);
+                    db.Remove(grado);
+                    db.SaveChanges();
+                    rpta = 1;
+                }
+            }
+            catch (Exception)
+            {
+
+                rpta = 0;
+            }
+            return rpta;
+        }
+
+
+
+
+        [HttpGet]
+        [Route("api/Grado/Filtrar/{data?}")]
+        public List<Grado> Filtrar(string data)
+        {
+            List<Grado> lista = new List<Grado>();
+            using (RegistroAcademicoContext db = new RegistroAcademicoContext())
+            {
+                if (data == null)
+                {
+                    lista = (from gd in db.Grado
+                             select new Grado
+                             {
+                                 GradoId = gd.GradoId,
+                                 GradoNombre = gd.GradoNombre,
+                                 Seccion = gd.Seccion
+                             }).ToList();
+                }
+
+                else
+                {
+                    lista = (from g in db.Grado
+                             where g.GradoId.ToString().Contains(data) || g.GradoNombre.Contains(data) ||
+                                   g.Seccion.Contains(data) 
+                             select new Grado
+                             {
+                                 GradoId = g.GradoId,
+                                 GradoNombre = g.GradoNombre,
+                                 Seccion = g.Seccion
+                             }).ToList();
+
+                }
+            }
+            return lista;
+
+        }
+
+
+        [HttpGet]
+        [Route("api/Grados/Get")]
+        public List<GradoCls> Get()
+        {
+            List<GradoCls> lista = new List<GradoCls>();
+            using (RegistroAcademicoContext db = new RegistroAcademicoContext())
+            {
+                lista = (from g in db.Grado
+                         select new GradoCls
+                         {
+                             GradoId = g.GradoId,
+                             GradoNombre = g.GradoNombre,
+                             Seccion = g.Seccion
+                         }).ToList();
+            }
+            return lista;
+
+        }
+
+
+
+    }
+}
---
+++ Server/Models/RegistroAcademicoContext.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace RegistroAcademicoApp.Server.Models
+{
+    public partial class RegistroAcademicoContext : DbContext
+    {
+        public RegistroAcademicoContext()
+        {
+        }
+
+        public RegistroAcademicoContext(DbContextOptions<RegistroAcademicoContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Cursos> Cursos { get; set; }
+        public virtual DbSet<DireccionEstudiantes> DireccionEstudiantes { get; set; }
+        public virtual DbSet<Estudiante> Estudiante { get; set; }
+        public virtual DbSet<Facultad> Facultad { get; set; }
+        public virtual DbSet<Grado> Grado { get; set; }
+        public virtual DbSet<Maestros> Maestros { get; set; }
+        public virtual DbSet<Seccion> Seccion { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("server=LAPTOP-74AJBVCC\\SQLEXPRESS;database=RegistroAcademico; Integrated Security=true");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Cursos>(entity =>
+            {
+                entity.HasKey(e => e.CursoId);
+
+                entity.HasIndex(e => e.OnLineMaestrosId);
+
+                entity.HasIndex(e => e.PresencialMaestrosId);
+
+                entity.Property(e => e.OnLineMaestrosId).HasColumnName("onLineMaestrosId");
+
+                entity.Property(e => e.PresencialMaestrosId).HasColumnName("presencialMaestrosID");
+
+                entity.HasOne(d => d.OnLineMaestros)
+                    .WithMany(p => p.CursosOnLineMaestros)
+                    .HasForeignKey(d => d.OnLineMaestrosId);
+
+                entity.HasOne(d => d.PresencialMaestros)
+                    .WithMany(p => p.CursosPresencialMaestros)
+                    .HasForeignKey(d => d.PresencialMaestrosId);
+            });
+
+            modelBuilder.Entity<DireccionEstudiantes>(entity =>
+            {
+                entity.HasKey(e => e.IdDireccionEstudiante);
+
+                entity.HasIndex(e => e.EstudianteIdEstudiante);
+
+                entity.HasOne(d => d.EstudianteIdEstudianteNavigation)
+                    .WithMany(p => p.DireccionEstudiantes)
+                    .HasForeignKey(d => d.EstudianteIdEstudiante);
+            });
+
+            modelBuilder.Entity<Estudiante>(entity =>
+            {
+                entity.HasKey(e => e.IdEstudiante);
+
+                entity.HasIndex(e => e.CursosCursoId);
+
+                entity.HasIndex(e => e.FacultadRefId);
+
+                entity.HasIndex(e => e.GradoId);
+
+                entity.Property(e => e.Altura).HasColumnType("decimal(18, 2)");
+
+                entity.Property(e => e.Nombre)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.RowVersion)
+                    .IsRowVersion()
+                    .IsConcurrencyToken();
+
+                entity.HasOne(d => d.CursosCurso)
+                    .WithMany(p => p.Estudiante)
+                    .HasForeignKey(d => d.CursosCursoId);
+
+                entity.HasOne(d => d.FacultadRef)
+                    .WithMany(p => p.Estudiante)
+                    .HasForeignKey(d => d.FacultadRefId);
+
+                entity.HasOne(d => d.Grado)
+                    .WithMany(p => p.Estudiante)
+                    .HasForeignKey(d => d.GradoId);
+            });
+
+            modelBuilder.Entity<Maestros>(entity =>
+            {
+                entity.HasKey(e => e.MaestroId);
+            });
+
+            modelBuilder.Entity<Seccion>(entity =>
+            {
+                entity.HasKey(e => e.IdSeccion);
+
+                entity.Property(e => e.Nombre)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
