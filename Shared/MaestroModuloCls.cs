3153b7cd0a126aeb154a80db60bcb789e1e8bdff
Carlos Enrique Lemus Serrano <cwlemus@gmail.com>
2021-04-10 14:26:15 -0600

    [10-04-2021:14:25] - Reporte de cursos por maestro

---
+++ Shared/MaestroModuloCls.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistroAcademicoApp.Shared
+{
+    public class MaestroModuloCls
+    {
+        public int Corr { get; set; }
+        public string NombreCurso { get; set; }
+    }
+}
